3e3b28226843e7f8354f7cb79bf063713ec55007
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-21 21:43:15 -0800

    find index of delete row

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/items', (req, res, next) => {
 	});
 });
 
-app.post('/items', (req, res, next) => {
+app.post('/items/add', (req, res, next) => {
 	console.log(req.body)
 	fs.readFile('./list.json', (err, data) => {
 		if (err) return res.status(400).send(err);
@@ -40,10 +40,23 @@ app.post('/items', (req, res, next) => {
 		arr.push({"item": item, "date": date});
 		fs.writeFile('./list.json', JSON.stringify(arr), (err, data) => {
 			if (err) return res.status(400).send(err);
-			res.send([item, date]);
+			res.send([item, date]); // TODO could be object
 		});
 	})
 });
+
+app.post('/items/delete', (req, res, next) => {
+	console.log(req.body)
+	fs.readFile('./list.json', (err, data) => {
+		if (err) return res.status(400).send(err);
+		var arr = JSON.parse(data);
+		console.log(arr);
+		fs.writeFile('./list.json', JSON.stringify(arr), (err, data) => {
+			if (err) return res.status(400).send(err);
+			res.send([item, date]);
+		});
+	})
+})
 
 // spin up server
 app.listen(PORT, () => {
--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,13 @@
 $(document).ready(function() {
 	populateItems();
 	$('#addItem').click(addItem);
+	$('body').on('click','.delete', deleteItem);
 });
 
 function addItem() {
 	var newItem = $('#item').val();
 	var newDate = $('#date').val();
-	$.post('./items', {
+	$.post('./items/add', {
 		item: newItem,
 		date: newDate
 	})
@@ -25,7 +26,6 @@ function addItem() {
 	});
 }
 
-
 function populateItems() {
 	$.get('./items', function(data) {
 		console.log(data);
@@ -38,3 +38,40 @@ function populateItems() {
 		}); 
 	})
 }
+
+function deleteItem() {
+	var index  = $(this).closest('tr').index() - 1;
+	console.log(index);
+
+	// $.post('./items/delete', {
+	// 	item: newItem,
+	// 	date: newDate
+	// })
+	// .success(function(data) {
+	// 	console.log(data);
+	// 	var $task = $('#template').clone();
+	// 	$task.removeAttr('id');
+	// 	$task.children('.itemOutput').text(data[0]);
+	// 	$task.children('.dateOutput').text(data[1]);
+	// 	$('#taskList').append($task);
+	// })
+	// .fail(function(err) {
+	// 	alert('something went wrong');
+	// });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
